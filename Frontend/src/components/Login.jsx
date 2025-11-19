fa0a3843e6780edb33559747343b85b2efc53270
Yaman Gahlout <yamangahlout123@gmail.com>
2025-11-19 22:49:33 +0530

    signup and login page created

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
-function Login() {
-  return <div>Login</div>;
+function Login({ setCurrentTab }) {
+  return (
+    <div>
+      <div className="flex mt-[100px] flex-col gap-5 w-[400px] h-[400px] shadow-2xl shadow-gray-900 border border-gray-200 rounded-2xl p-5 items-center">
+        <h1 className="text-5xl  text-gray-200">Login</h1>
+        <form className="flex flex-col gap-5 mt-8 items-center">
+          <input
+            type="email"
+            placeholder="Enter email"
+            className="border border-gray-200 rounded-md w-[350px] p-[10px_20px] bg-gray-200 text-black placeholder:text-black outline-none"
+          />
+          <input
+            type="password"
+            placeholder="Enter password"
+            className="border border-gray-200 rounded-md w-[350px] p-[10px_20px] bg-gray-200 text-black placeholder:text-black outline-none"
+          />
+          <button
+            //onClick={() => navigate("/signup")}
+            className="mt-5 p-[10px_20px] border cursor-pointer border-gray-200 w-full text-gray-200 rounded-lg"
+          >
+            Login
+          </button>
+          <p className="text-lg text-gray-200 opacity-80">
+            New to openPost?{" "}
+            <a
+              onClick={() => setCurrentTab("Signup")}
+              className="text-blue-700 cursor-pointer"
+            >
+              Register
+            </a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- Frontend/src/components/Main.jsx
+++ Frontend/src/components/Main.jsx
@@ -1,39 +1,39 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
+import { useState } from "react";
 
 function Main() {
   const navigate = useNavigate("");
+  const [currentTab, setCurrentTab] = useState("Login");
   return (
     <div className="h-screen w-screen bg-gray-950">
       <div className="w-full flex justify-center p-5">
         <div className="w-[85%] flex justify-between items-center">
           <h1 className="text-3xl text-gray-300 cursor-pointer">openPost.</h1>{" "}
-          <div className="flex w-[15%] justify-between">
-            <button
-              onClick={() => navigate("/signup")}
-              className="p-[10px_20px] border cursor-pointer border-gray-200 text-gray-200 rounded-lg"
-            >
-              Signup
-            </button>
-            <button
-              onClick={() => navigate("/login")}
-              className="p-[10px_20px] border cursor-pointer border-gray-200 bg-gray-300 rounded-lg"
-            >
-              Login
-            </button>
+          <div className="">
+            {currentTab === "Signup" && (
+              <button
+                onClick={() => setCurrentTab("Login")}
+                className="p-[10px_20px] border cursor-pointer border-gray-200 text-gray-200 rounded-lg hover:text-black hover:bg-gray-200 transition-all ease-in-out duration-700"
+              >
+                Login
+              </button>
+            )}
+            {currentTab === "Login" && (
+              <button
+                onClick={() => setCurrentTab("Signup")}
+                className="p-[10px_20px] border cursor-pointer border-gray-200 text-gray-200 rounded-lg hover:text-black hover:bg-gray-200 transition-all ease-in-out duration-700"
+              >
+                Signup
+              </button>
+            )}
           </div>
         </div>
       </div>
       <div className="w-full flex justify-center p-5">
-        <div className="flex flex-col justify-center items-center gap-[50px] mt-[100px]">
-          <div className="flex justify-center items-center gap-7">
-            <div className="h-[250px] w-[350px] bg-white rounded-2xl"></div>
-            <div className="h-[250px] w-[350px] bg-white rounded-2xl"></div>
-          </div>
-          <button className="p-[10px_20px] w-[200px] border cursor-pointer border-gray-200 text-gray-200 rounded-lg">
-            Get started
-          </button>
-        </div>
+        {currentTab === "Login" && <Login setCurrentTab={setCurrentTab} />}
+        {currentTab === "Signup" && <Signup setCurrentTab={setCurrentTab} />}
       </div>
     </div>
   );
