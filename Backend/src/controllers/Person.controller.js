0c5b0d2a7f8965b48d06632208da5e19edc7a410
Vibahvasu Bhadauria <vibhavasudsvv2003@gmail.com>
2025-11-20 23:58:03 +0530

    21-11/5

--- Backend/src/controllers/Person.controller.js
+++ Backend/src/controllers/Person.controller.js
@@ -2,14 +2,26 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import { db } from "../db/index.js";
-import { hashPassword,comparePassword } from "../utils/hashPassword.js";
+import { hashPassword, comparePassword } from "../utils/hashPassword.js";
 import { createToken } from "../utils/jwtCreator.js";
 //import { dobToAgeFinder } from "../utils/dobToAge.js";
 
 const registerPerson = async (req, res) => {
-  const {person_id,person_username,person_fname,person_lname,person_dob,person_gender,person_password,} = req.body;
+  const {
+    person_username,
+    person_fname,
+    person_lname,
+    person_dob,
+    person_gender,
+    person_password,
+  } = req.body;
   if (
-    !person_id ||!person_username ||!person_fname ||!person_lname ||!person_dob ||!person_gender ||!person_password
+    !person_username ||
+    !person_fname ||
+    !person_lname ||
+    !person_dob ||
+    !person_gender ||
+    !person_password
   ) {
     throw new ApiError(400, "All fields are required");
   }
@@ -28,11 +40,18 @@ const registerPerson = async (req, res) => {
   if (existedUser.length > 0) {
     throw new ApiError(409, "User allready exists");
   }
-const hashedPassword =  await hashPassword(person_password);
-  try{
-    const [p]=await db.execute(
-      `INSERT INTO Persons (person_id, person_username, person_fname, person_lname, person_dob, person_gender, person_password) VALUES (?,?,?,?,?,?,?)`,
-      [person_id,person_username,person_fname,person_lname,person_dob,person_gender,hashedPassword]
+  const hashedPassword = await hashPassword(person_password);
+  try {
+    const [p] = await db.execute(
+      `INSERT INTO Person(person_username, person_fname, person_lname, person_dob, person_gender, person_password) VALUES (?,?,?,?,?,?)`,
+      [
+        person_username,
+        person_fname,
+        person_lname,
+        person_dob,
+        person_gender,
+        hashedPassword,
+      ]
     );
     res
       .status(201)
@@ -42,26 +61,29 @@ const hashedPassword =  await hashPassword(person_password);
   }
 };
 const loginPerson = async (req, res) => {
-    const { person_username, person_password } = req.body;
-    if (!person_username || !person_password) {
-      throw new ApiError(400, "All fields are required");
-    }
-    let [user] = await db.execute(
-      `SELECT * FROM Persons WHERE person_username = ?`,
-      [person_username]
-    );
-    if (user.length === 0) {
-      throw new ApiError(404, "User not found");
-    }
-    const isPasswordValid = await comparePassword(person_password, user[0].person_password);
-    if (!isPasswordValid) {
-      throw new ApiError(401, "Invalid password");
-    }
-    const token = createToken(user[0].person_id, user[0].person_username);
-    const options = {
+  const { person_username, person_password } = req.body;
+  if (!person_username || !person_password) {
+    throw new ApiError(400, "All fields are required");
+  }
+  let [user] = await db.execute(
+    `SELECT * FROM Person WHERE person_username = ?`,
+    [person_username]
+  );
+  if (user.length === 0) {
+    throw new ApiError(404, "User not found");
+  }
+  const isPasswordValid = await comparePassword(
+    person_password,
+    user[0].person_password
+  );
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid password");
+  }
+  const token = createToken(user[0].person_username);
+  console.log("Generated Token: ", token);
+  const options = {
     httpOnly: true,
-    secure: false,
-    sameSite: "Lax",
+    secure: true,
   };
   return res
     .status(200)
