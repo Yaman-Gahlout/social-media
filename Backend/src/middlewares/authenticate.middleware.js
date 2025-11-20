a81c74ec9e5ddd6c7287b1d53e31dfa30aa44e4a
Vibahvasu Bhadauria <vibhavasudsvv2003@gmail.com>
2025-11-20 23:20:24 +0530

    20-11/4

--- Backend/src/middlewares/authenticate.middleware.js
+++ Backend/src/middlewares/authenticate.middleware.js
@@ -1,51 +1,20 @@
-import { User } from "../models/user.model.js";
-import { Trainer } from "../models/trainer.model.js"
-import { Admin } from "../models/admin.model.js"
 import jwt from "jsonwebtoken";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 
-const verifyUser =asyncHandler( async (req, res, next) => {
-    
-        const token = req.cookies?.userAccessToken;
-        if (!token) {
-            throw new ApiError(401, "Token not found");
-        }
-        const decordedToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        const user=await User.findById(decordedToken._id);
-        if(!user){
-            throw new ApiError(401,"Unauthorized access");
-        }
-        req.user=user;
-        next();
-    
-});
-const verifyTrainer=asyncHandler(async (req, res, next) => {
-    const token = req.cookies?.trainerAccessToken;
+const verifyPerson=asyncHandler(async (req, res, next) => {
+    const token = req.cookies?.personToken;
     if (!token) {
         throw new ApiError(401, "Token not found");
     }
     const decordedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    const trainer=await Trainer.findById(decordedToken._id);
-    if(!trainer){
+    const person=decordedToken.person_id;
+    if(!person){
         throw new ApiError(401,"Unauthorized access");
     }
-    req.trainer=trainer;
+    req.person=person;
     next();
 });
 
-const verifyAdmin=asyncHandler(async (req, res, next) => {
-    const token = req.cookies?.adminAccessToken;
-    if (!token) {
-        throw new ApiError(401, "Token not found");
-    }
-    const decordedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    const admin=await Admin.findById(decordedToken._id);
-    if(!admin){
-        throw new ApiError(401,"Unauthorized access");
-    }
-    req.admin=admin;
-    next();
-});
-export {verifyUser,verifyTrainer,verifyAdmin};
+export {verifyPerson};
